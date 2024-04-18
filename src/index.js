011704d1153781208586604d2a210fe70d50b491
Neil Bizzell <NBizzell@users.noreply.github.com>
2024-04-18 12:48:22 +0100

    forgotten password

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const { v4: uuidv4 } = require("uuid");
 const multer = require ("multer");
 const {google} = require ("googleapis");
 const fs = require("fs");
-const {nodemailer} = require ("nodemailer");
-const {jwt} = require ("jsonwebtoken");
+const nodemailer = require ("nodemailer");
+const jwt = require ("jsonwebtoken");
 
 const port = process.env.PORT; // port is now equal to PORT from .env
 const dburi = process.env.DBURI; // dburi is now equal to DBURI from .env
@@ -350,7 +350,7 @@ app.post("/forgetPassword", async (req, res) => {
 
 	try {
     // Find the user by email
-    const user = await User.findOne({ mail: req.body.email });
+    const user = await User.findOne({ email: req.body.email });
 
     // If user not found, send error message
     if (!user) {
@@ -359,24 +359,31 @@ app.post("/forgetPassword", async (req, res) => {
 
     // Generate a unique JWT token for the user that contains the user's id
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {expiresIn: "10m",});
-
+    
     // Send the token to the user's email
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD_APP_EMAIL,
-      },
-    });
+			host: "smtp.ionos.co.uk",
+			port: 587,
+			secure: false, 
+			auth: {
+				user: process.env.EMAIL,
+				pass: process.env.PASSWORD_APP_EMAIL,
+			},
+			tls: {
+        ciphers:'SSLv3'
+    	},
+			from: process.env.EMAIL,
+		});
 
     // Email configuration
     const mailOptions = {
-      from: process.env.EMAIL,
+      from: 'Stannington Carnival Password Reset <info@stanningtoncarnival.co.uk>',
       to: req.body.email,
       subject: "Reset Password",
+			text: 'this email was sent to allow a password reset',
       html: `<h1>Reset Your Password</h1>
     <p>Click on the following link to reset your password:</p>
-    <a href="http://localhost:5173/reset-password/${token}">http://localhost:5173/reset-password/${token}</a>
+    <a href="http://localhost:3000/reset-password/${token}">http://localhost:3000/reset-password/${token}</a>
     <p>The link will expire in 10 minutes.</p>
     <p>If you didn't request a password reset, please ignore this email.</p>`,
     };
@@ -384,6 +391,7 @@ app.post("/forgetPassword", async (req, res) => {
     // Send the email
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
+				console.log(token)
         return res.status(500).send({ message: err.message });
       }
       res.status(200).send({ message: "Email sent" });
@@ -394,7 +402,38 @@ app.post("/forgetPassword", async (req, res) => {
 });
 
 app.post("/reset-password/:token", async (req, res) => {
+	try {
+    // Verify the token sent by the user
+    const decodedToken = jwt.verify(
+      req.params.token,
+      process.env.JWT_SECRET_KEY
+    );
 
+    // If the token is invalid, return an error
+    if (!decodedToken) {
+      return res.status(401).send({ message: "Invalid token" });
+    }
+
+    // find the user with the id from the token
+    const user = await User.findOne({ _id: decodedToken.userId });
+    if (!user) {
+      return res.status(401).send({ message: "no user found" });
+    }
+    
+    // Hash the new password
+    const salt = await bycrypt.genSalt(10);
+    req.body.newPassword = await bycrypt.hash(req.body.newPassword, salt);
+
+    // Update user's password, clear reset token and expiration time
+    user.password = req.body.newPassword;
+    await user.save();
+
+    // Send success response
+    res.status(200).send({ message: "Password updated" });
+  } catch (err) {
+    // Send error response if any error occurs
+    res.status(500).send({ message: err.message });
+  }
 });
 
 
